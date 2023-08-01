c738b8138f5469d85e7f2dfd78692e3870b62ccf
amir58 <devamir11@gmail.com>
2023-08-01 19:29:10 +0300

    Session2

--- lib/basics.dart
+++ lib/basics.dart
@@ -102,4 +102,129 @@ main() {
   print(numberTwo == numberThree); // Equal To
   print(numberTwo != numberThree); // Not Equal To
 
+  print('---------------------');
+  // && || !
+  // += -= *= /= %=
+  // ? ! ?? ??= ( OOP )
+  // ? :
+  // is
+
+  int numOne = 16;
+
+  //     true  && true
+  print(numOne > 5 && numOne < 15);
+  print(numOne > 5 || numOne < 15);
+
+  print('-----------');
+  // ! = not
+  bool test = false;
+  print(!test); // not false
+
+  print(!true);
+  print(!false);
+
+  print('-----------');
+  // += -= *= /= %=
+  double num1 = 10;
+  num1 = num1 + 5;
+  num1 += 5;
+  print(num1);
+
+  // num1 = num1 * 5;
+  num1 *= 5;
+  print(num1);
+
+  // num1 = num1 / 5;
+  num1 /= 4;
+  print(num1);
+
+  // num1 = num1 - 5;
+  num1 -= 5;
+  print(num1);
+
+  // num1 = num1 % 5;
+  num1 %= 5;
+  print(num1);
+
+  print('---------------');
+  // is is!
+
+  print(num1 is! int);
+  print(num1 is String);
+  print(num1 is double);
+
+  print('---------------');
+  // ? :
+  //             condition ? resultOne  : resultTwo
+  String message = 10 > 50 ? "Yes" : "No";
+  print(message);
+  int messageI = 10 > 5 ? 1 : 0;
+  print(messageI);
+  // condition ? Colors.green : Colors.red
+  // condition ? Icons.success : Icons.failure
+
+  print('----------------------');
+  // if condition , switch case
+
+  // if ( condition ){ body } else { body }
+
+  if (5 > 10) {
+    print('5 Big than 10');
+  } else {
+    print('5 Smaller than 10');
+  }
+
+  int a = 11;
+  if (a > 1 && a < 10) {
+    print('a is a valid number');
+  } else {
+    print('a is a not valid number');
+  }
+
+  print("amir" == "amir");
+  print("Amir" == "amir");
+
+  // requested , accepted , cancelled
+  String orderStatus = "cancelled";
+  if (orderStatus == "requested") {
+    print('Order requested!');
+  } else if (orderStatus == "accepted") {
+    print('Order accepted!');
+  } else {
+    print('Order status unknown!');
+  }
+
+  // switch ( variable ) { case one : { body } break; }
+  switch (orderStatus) {
+    case "requested":
+      {
+        print('Order requested!');
+      }
+      break;
+    case "accepted":
+      {
+        print('Order accepted!');
+      }
+      break;
+    default:
+      {
+        print('Order status unknown!');
+      }
+  }
+
+  print('-------------------');
+  int dayOfWeek = 3;
+  switch (dayOfWeek) {
+    case 1:
+      print('Saturday');
+      break;
+    case 2:
+      print('Sunday');
+      break;
+    case 3:
+      print('Monday');
+      break;
+    default:
+      print('Not valid number');
+  }
 }
